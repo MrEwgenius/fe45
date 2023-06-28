b09487edbd8b32b3973154b9820b92f244fad90e
Bolynskii Ewgenii <119881162+MrEwgenius@users.noreply.github.com>
2023-06-28 20:54:02 +0300

    [HW-2] add new component: bookmark, dislikeIcon, likeIkon, morHorizontal, Posts (#2)

---
+++ src/components/Posts/Posts.tsx
@@ -0,0 +1,66 @@
+import React, { FC } from 'react';
+import styles from './Posts.module.scss'
+import LikeIcons from '../../assets/icons/LikeIcons';
+import DislikeIcon from '../../assets/icons/DislikeIcons';
+import Bookmark from '../../assets/icons/Bookmark';
+import MoreHorizontal from '../../assets/icons/MoreHorizontal';
+import classNames from 'classnames';
+
+export enum PostsTypes {
+    Large = 'large',
+    Medium = 'medium',
+    Small = 'small',
+
+}
+type PostsProps = {
+    id: PostsTypes,
+    image?: string,
+    text?: string,
+    date?: string,
+    lesson_num?: number,
+    title: string,
+    author?: number,
+}
+
+
+
+const Posts: FC<PostsProps> = ({
+    title,
+    id,
+    date,
+    text,
+    image,
+}) => {
+
+    const postsStyle = styles[id]
+
+
+
+    return (
+        <div className={classNames(postsStyle)}>
+            <div className={styles.postWrap}>
+                <div className={styles.postContainer}>
+                    <div className={styles.postDate}>{date}</div>
+                    <div className={styles.postTitle}>{title}</div>
+                    {id === PostsTypes.Large && (
+                        <div className={styles.postText}>{text}</div>
+                    )}
+                </div>
+                <img className={styles.postImg} src={image} alt="image" />
+
+            </div>
+            <div className={styles.iconContainerWrap}>
+                <div className={styles.likeDislikeContainer}>
+                    <LikeIcons />
+                    <DislikeIcon />
+                </div>
+                <div className={styles.bookmarkMoreHorizontalContainer}>
+                    <Bookmark />
+                    <MoreHorizontal />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Posts;
